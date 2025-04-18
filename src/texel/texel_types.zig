a97b34f3e404395d1df5ca3aefa16eaaf8cf7eb3
Asgard <git@asgard.io>
2025-04-18 01:10:09 -0600

    Sensible defaults
    Former-commit-id: 509cc97e95f90222692e8a34671203b210c0029b

--- src/texel/texel_types.zig
+++ src/texel/texel_types.zig
@@ -1,8 +1,8 @@
 pub const RGBA8U = extern struct {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
+    a: u8 = 255,
 
     pub fn byteSizeForTexels(width: u32, height: u32) usize {
         return @sizeOf(RGBA8U) * width * height;
@@ -10,10 +10,10 @@ pub const RGBA8U = extern struct {
 };
 
 pub const RGBA16U = extern struct {
-    r: u16,
-    g: u16,
-    b: u16,
-    a: u16,
+    r: u16 = 0,
+    g: u16 = 0,
+    b: u16 = 0,
+    a: u16 = 1.0,
 
     pub fn byteSizeForTexels(width: u32, height: u32) usize {
         return @sizeOf(RGBA16U) * width * height;
@@ -21,10 +21,10 @@ pub const RGBA16U = extern struct {
 };
 
 pub const RGBA16F = extern struct {
-    r: f16,
-    g: f16,
-    b: f16,
-    a: f16,
+    r: f16 = 0,
+    g: f16 = 0,
+    b: f16 = 0,
+    a: f16 = 1.0,
 
     pub fn byteSizeForTexels(width: u32, height: u32) usize {
         return @sizeOf(RGBA16F) * width * height;
