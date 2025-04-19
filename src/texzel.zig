eee33ef485e306f2641591ce97b003c2588c53cf
Asgard <git@asgard.io>
2025-04-18 20:58:20 -0600

    Add some docs
    Former-commit-id: 115ec14aab3a3ac89ee41ecd3de7d17bf2f28ab9

--- src/texzel.zig
+++ src/texzel.zig
@@ -1,3 +1,10 @@
+//! A texture encoding/decoding library for Zig.
+//!
+//! This library provides a set of functions to encode and decode textures in various formats.
+//!
+//! For BCn formats, see `block`.
+//! For raw texel formats, see `texel`.
+
 pub const texel = @import("texel.zig");
 pub const utils = @import("utils.zig");
 pub const block = @import("block.zig");
